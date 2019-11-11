48d8cb42467b12ca681a51ffc6710fd0fd94e20b
hjälte <hjalte79@gmail.com>
2019-11-11 22:40:10 +0100

    Added initial files

---
+++ BundleSameParts/OccurrenceBundler.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventor;
+
+namespace Hjalte.OccurrenceBundler
+{
+    public class OccurrenceBundler
+    {
+        private ButtonDefinition bundleButtonDefinition;
+        private Application inventor;
+        private const string MODELBROWSERNAME = "AmBrowserArrangement";
+
+        public OccurrenceBundler(Application inventor, string GUID)
+        {
+            this.inventor = inventor;
+
+            bundleButtonDefinition = inventor.CommandManager.ControlDefinitions.AddButtonDefinition(
+                    "Bundle same files in folder.", "Hjalte.OccurrenceBundler.handler",
+                    CommandTypesEnum.kSchemaChangeCmdType, GUID,
+                    "Bundle same files in folder.", "Bundle same files in folder.");
+            bundleButtonDefinition.OnExecute += onClickBundle;
+
+
+        }
+        public void OnContextMenu(SelectionDeviceEnum SelectionDevice, NameValueMap AdditionalInfo, CommandBar CommandBar)
+        {
+            Document oDoc = inventor.ActiveDocument;
+            if (!(oDoc is AssemblyDocument)) return;
+            AssemblyDocument doc = (AssemblyDocument)oDoc;
+
+
+            BrowserNode selectedNode = getSelectedNode(doc.BrowserPanes[MODELBROWSERNAME].TopNode);
+
+            if (selectedNode == null) return;
+
+            try
+            {
+                if (selectedNode.NativeObject is ComponentOccurrence)
+                {
+                    CommandBar.Controls.AddButton(bundleButtonDefinition, 6);
+                }
+                else if (selectedNode.NativeObject is BrowserFolder)
+                {
+                    CommandBar.Controls.AddButton(bundleButtonDefinition, 5);
+                }
+            }
+            catch (Exception) { }            
+        }
+        private void onClickBundle(NameValueMap Context)
+        {
+            Document oDoc = inventor.ActiveDocument;
+            if (!(oDoc is AssemblyDocument)) return;
+
+            AssemblyDocument doc = (AssemblyDocument)oDoc;
+            BrowserPane browserPane = doc.BrowserPanes[MODELBROWSERNAME];
+            BrowserNode topNode = browserPane.TopNode;
+
+            BrowserNode selectedNode = getSelectedNode(topNode);
+            if (selectedNode == null) return;
+
+            string fileToBundle = "";
+            if (selectedNode.NativeObject is ComponentOccurrence)
+            {
+                ComponentOccurrence occ = (ComponentOccurrence)selectedNode.NativeObject;
+                fileToBundle = occ.ReferencedFileDescriptor.FullFileName;
+                                
+                BrowserFolder folder = getFolderForFile(browserPane, fileToBundle);
+
+                List<BrowserNode> nodesWithFileName = getNodesWithFileName(fileToBundle, topNode);
+                foreach (BrowserNode foundNode in nodesWithFileName)
+                {
+                    folder.Add(foundNode);
+                }
+                folder.Name = nodesWithFileName.Count + "x " + System.IO.Path.GetFileNameWithoutExtension(fileToBundle);
+            }
+            else if (selectedNode.NativeObject is BrowserFolder)
+            {
+                BrowserFolder folder = (BrowserFolder)selectedNode.NativeObject;
+                
+                if (folder.BrowserNode.BrowserNodes[1].NativeObject is ComponentOccurrence)
+                {
+                    ComponentOccurrence occ = (ComponentOccurrence)folder.BrowserNode.BrowserNodes[1].NativeObject;
+                    fileToBundle = occ.ReferencedFileDescriptor.FullFileName;
+
+                    folder.Delete();
+                    folder = getFolderForFile(browserPane, fileToBundle);
+
+                    List<BrowserNode> nodesWithFileName = getNodesWithFileName(fileToBundle, topNode);
+                    foreach (BrowserNode foundNode in nodesWithFileName)
+                    {
+                        folder.Add(foundNode);
+                    }
+                    folder.Name = nodesWithFileName.Count + "x " + System.IO.Path.GetFileNameWithoutExtension(fileToBundle);
+                }
+            }
+            
+
+        }
+
+        private BrowserFolder getFolderForFile(BrowserPane browserPane, string searchedFileName)
+        {
+            BrowserFolder folder = null;
+
+            foreach (BrowserFolder curentFolder in browserPane.TopNode.BrowserFolders)
+            {
+                foreach (BrowserNode node in curentFolder.BrowserNode.BrowserNodes)
+                {
+                    if (node.NativeObject is ComponentOccurrence)
+                    {
+                        ComponentOccurrence occ = (ComponentOccurrence)node.NativeObject;
+                        string currentFileName = occ.ReferencedFileDescriptor.FullFileName;
+                        if (!searchedFileName.Equals(currentFileName, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    folder = curentFolder;
+                    break;
+                }
+            }
+
+            if (folder == null )
+            {
+                ObjectCollection colNodes = inventor.TransientObjects.CreateObjectCollection();
+                folder = browserPane.AddBrowserFolder("temp", colNodes);
+            }
+            
+            return folder;
+        }
+
+        private List<BrowserNode> getNodesWithFileName(string fileName, BrowserNode topNode)
+        {
+            List<BrowserNode> nodes = new List<BrowserNode>();
+            foreach (BrowserNode node in topNode.BrowserNodes)
+            {
+                if (node.NativeObject is ComponentOccurrence)
+                {
+                    ComponentOccurrence occ = (ComponentOccurrence)node.NativeObject;
+                    string foundOccfilename = occ.ReferencedFileDescriptor.FullFileName;
+                    if (fileName.Equals(foundOccfilename))
+                    {
+                        nodes.Add(node);
+                    }
+                }
+                if (node.NativeObject is BrowserFolder)
+                {
+                    BrowserFolder folder = (BrowserFolder)node.NativeObject;
+                    nodes.AddRange(getNodesWithFileName(fileName, folder.BrowserNode));
+                }
+            }
+            return nodes;
+        }
+
+        private BrowserNode getSelectedNode(BrowserNode topNode)
+        {
+            BrowserNode foundNode = null;
+            foreach (BrowserNode node in topNode.BrowserNodes)
+            {
+                if (node.Selected) return node;
+                foundNode = getSelectedNode(node);
+                if (foundNode != null)
+                {
+                    return foundNode;
+                }
+            }
+            return foundNode;
+        }
+
+
+    }
+}
---
+++ BundleSameParts/StandardAddInServer.cs
@@ -0,0 +1,75 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) Hjälte. All rights reserved 
+// Written by Jelte de Jong - 2019
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted, 
+// provided that the above copyright notice appears in all copies and 
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting 
+// documentation.
+//
+// HJALTE PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// HJALTE SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Inventor;
+
+namespace Hjalte.OccurrenceBundler
+{
+    [GuidAttribute("1cecf0ae-ce16-4473-9b8b-320842f927f3"), ComVisible(true)]
+    public class StandardAddInServer : ApplicationAddInServer
+    {
+        //https://forums.autodesk.com/t5/inventor-ideas/bundle-multiple-instances-of-same-part-in-assembly-tree/idi-p/5643016
+
+        private Application inventor;
+        private OccurrenceBundler occurrenceBundler;
+        public void Activate(ApplicationAddInSite addInSiteObject, bool firstTime)
+        {
+            inventor = addInSiteObject.Application;
+
+            occurrenceBundler = new OccurrenceBundler(inventor, this.getGUID());
+            inventor.CommandManager.UserInputEvents.OnContextMenu += occurrenceBundler.OnContextMenu;
+
+        }
+
+        private string getGUID()
+        {
+            GuidAttribute addInCLSID = (GuidAttribute)GuidAttribute.GetCustomAttribute(
+                typeof(StandardAddInServer),
+                typeof(GuidAttribute));
+            return "{" + addInCLSID.Value + "}";
+        }
+
+        public void Deactivate()
+        {
+            // Release objects.
+            Marshal.ReleaseComObject(inventor);
+            inventor = null;
+
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+        public void ExecuteCommand(int commandID)
+        {
+            // Note:this method is now obsolete, you should use the 
+            // ControlDefinition functionality for implementing commands.
+        }
+        public object Automation
+        {
+            get
+            {
+                // TODO: Add ApplicationAddInServer.Automation getter implementation
+                return null;
+            }
+        }
+    }
+}
